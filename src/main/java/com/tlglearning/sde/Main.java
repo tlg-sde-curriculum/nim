9d007137d4321e80322739899bea4f26179b39c5
nick-bennett <nick@nickbenn.com>
2022-12-10 18:31:53 -0700

    Adjust template; include main class.

---
+++ src/main/java/com/tlglearning/sde/Main.java
@@ -0,0 +1,9 @@
+package com.tlglearning.sde;
+
+public class Main {
+
+  public static void main(String[] args) {
+    // TODO: 2022-12-10 Start a (potentially) multi-game session and run to completion.
+  }
+  
+}
